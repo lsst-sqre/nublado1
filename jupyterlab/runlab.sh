4b8dd7dc83ef9fb8895a79ab6a2b9bb86cfabf6d
adam <athornton@gmail.com>
2017-04-27 11:56:16 -0700

    Move notebook dir to "notebooks", change user setup to start in notebooks.

--- jupyterlab/runlab.sh
+++ jupyterlab/runlab.sh
@@ -6,7 +6,7 @@ pwd
 cmd="python /usr/bin/jupyter-singlelabuser \
      --ip='*' --port=8888 --debug \
      --hub-api-url=${JPY_HUB_API_URL} \
-     --notebook-dir=${HOME}/data \
+     --notebook-dir=${HOME}/notebooks \
      --LabApp.base_url=/user/${USER}"
 echo ${cmd}
 exec ${cmd}

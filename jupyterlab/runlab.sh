b122e29d59a55d4f5598353ca79041a180688820
adam <athornton@gmail.com>
2017-05-04 13:42:43 -0700

    The problem is down in commandpalette.

--- jupyterlab/runlab.sh
+++ jupyterlab/runlab.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+export DEBUG=1
+if [ -n "${DEBUG}" ]; then
+    set -x
+fi
 sync
 cd ${HOME}
 pwd
@@ -6,7 +10,8 @@ cmd="python3 /usr/bin/jupyter-singlelabuser \
      --ip='*' --port=8888 --debug \
      --hub-api-url=${JPY_HUB_API_URL} \
      --notebook-dir=${HOME}/notebooks \
-     --LabApp.base_url=/user/${USER}"
+     --LabApp.base_url=/user/${USER} \
+     --cookie-name=user-${USER}"
 echo ${cmd}
 if [ -n "${DEBUG}" ]; then
     ${cmd}

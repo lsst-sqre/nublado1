359ccb349b40c08ed1eb310d859b6c2d609090ad
adam <athornton@gmail.com>
2017-05-09 10:04:21 -0700

    Allowing NFS access to all hosts in cluster. Created NFS-keepalive container. Added Filebeat Daemonset.

--- fileserver/substitute-network.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-CLUSTERNET="$(ip addr show | grep 'inet ' | grep 'scope global' | \
-		 awk '{print $2}' | cut -d '.' -f 1-2).0.0/16"
-
-sed -e "s|{{CONTAINERS}}|${CLUSTERNET}|" < /tmp/exports.template > /etc/exports
-

14ff0db544110ac5ded2938ce82ffc4835faa5f0
adam <athornton@gmail.com>
2017-07-12 17:13:07 -0700

    Got rid of need for "assets" directories.

--- jupyterlab/runlab.sh
+++ jupyterlab/runlab.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export DEBUG=1
+#export DEBUG=1
 if [ -n "${DEBUG}" ]; then
     set -x
 fi
@@ -10,14 +10,14 @@ cmd="python3 /usr/bin/jupyter-singlelabuser \
      --ip='*' --port=8888 --debug \
      --hub-api-url=${JPY_HUB_API_URL} \
      --notebook-dir=${HOME}/notebooks \
-     --LabApp.base_url=/user/${USER}"
+     --allow-root
+     --base_url=/user/${USER}"
 echo ${cmd}
 if [ -n "${DEBUG}" ]; then
     while : ; do
-	${cmd}
-	d=$(date)
-	echo "${d}: sleeping."
-	sleep 60
+        d=$(date)
+        echo "${d}: sleeping."
+        sleep 60
     done
 else    
     exec ${cmd}

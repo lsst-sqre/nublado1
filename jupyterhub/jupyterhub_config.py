518f78fb08e450566229fe11a0e396f93b8edb08
adam <athornton@gmail.com>
2017-08-12 15:28:13 -0700

    Subclass LoginHandler and make current again.

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -66,6 +66,10 @@ def _api_headers(access_token):
             }
 
 
+class LSSTLoginHandler(oauthenticator.GitHubLoginHandler):
+    scope = ['public_repo', 'read:org', 'user:email']
+
+
 class LSSTAuth(oauthenticator.GitHubOAuthenticator):
     """Authenticator to use our custom environment settings.
     """
@@ -73,29 +77,7 @@ class LSSTAuth(oauthenticator.GitHubOAuthenticator):
 
     _state = None
 
-    def get_state(self):
-        next_url = self.get_argument('next', None)
-        if self._state is None:
-            self._state = json.dumps({
-                'state_id': uuid.uuid4().hex,
-                'next_url': next_url,
-            })
-        self.log.info("State: %s" % self._state)
-        return self._state
-
-    def get_state_url(self):
-        """Get OAuth state from URL parameters
-        to be compared with the value in cookies
-        """
-        return self.get_argument("state")
-
-    def get_next_url(self):
-        """New one inherited from parent doesn't work?
-        Get the redirect target from the state field"""
-        state = self.get_state_url()
-        if state:
-            self.log.info("State (get_next_url): %s" % state)
-            return json.loads(state).get('next_url')
+    login_handler = LSSTLoginHandler
 
     @gen.coroutine
     def pre_spawn_start(self, user, spawner):
@@ -161,7 +143,7 @@ class LSSTAuth(oauthenticator.GitHubOAuthenticator):
         if not self.enable_auth_state:
             return
         auth_state = yield user.get_auth_state()
-        gh_id = auth_state.get("uid")
+        gh_id = auth_state.get("id")
         gh_token = auth_state.get("access_token")
         gh_org = yield self._get_user_organizations(gh_token)
         gh_email = auth_state.get("email")
@@ -171,7 +153,7 @@ class LSSTAuth(oauthenticator.GitHubOAuthenticator):
             spawner.environment['GITHUB_EMAIL'] = gh_email
         gh_name = auth_state.get("name")
         if not gh_name:
-            gh_name = auth_state.get("username")
+            gh_name = auth_state.get("login")
         if gh_id:
             spawner.environment['GITHUB_ID'] = str(gh_id)
         if gh_org:
@@ -341,7 +323,6 @@ c.JupyterHub.authenticator_class = LSSTAuth
 c.JupyterHub.spawner_class = LSSTSpawner
 
 # Set up auth environment
-c.LSSTAuth.scope = ['public_repo', 'read:org', 'user:email']
 c.LSSTAuth.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
 c.LSSTAuth.client_id = os.environ['GITHUB_CLIENT_ID']
 c.LSSTAuth.client_secret = os.environ['GITHUB_CLIENT_SECRET']

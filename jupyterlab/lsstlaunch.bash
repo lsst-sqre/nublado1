bd09d0787b80fbf54582cfe34bc52c2197cd8acd
adam <athornton@gmail.com>
2017-08-22 17:06:05 -0700

    Add Firefly, fix prepuller.

--- jupyterlab/lsstlaunch.bash
+++ jupyterlab/lsstlaunch.bash
@@ -3,6 +3,7 @@ PYTHON_VER=$1
 CONFIG_FILE=$2
 source /opt/lsst/software/stack/loadLSST.bash
 setup lsst_distrib
+setup display_firefly
 if [ -e ${HOME}/notebooks/.user_setups ]; then
     source ${HOME}/notebooks/.user_setups
 fi

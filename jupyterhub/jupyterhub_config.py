357dee777e2a244788944a8642f17d69ab516b81
adam <athornton@gmail.com>
2017-09-19 16:59:43 -0700

    Working on CILogon.

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -49,41 +49,28 @@ for idx, img in enumerate(imagelist):
     optform += " value=\"%s\">%s<br>\n" % (img, imgdesc)
 # Options form built.
 
-# Utility definitions from GitHub OAuthenticator.
-
-# Support github.com and github enterprise installations
-GITHUB_HOST = os.environ.get('GITHUB_HOST') or 'github.com'
-if GITHUB_HOST == 'github.com':
-    GITHUB_API = 'api.github.com'
-else:
-    GITHUB_API = '%s/api/v3' % GITHUB_HOST
-
-
-def _api_headers(access_token):
-    return {"Accept": "application/json",
-            "User-Agent": "JupyterHub",
-            "Authorization": "token {}".format(access_token)
-            }
-
-
 # Request additional scope on GitHub token to auto-provision magic in the
 # user container.
-class LSSTLoginHandler(oauthenticator.GitHubLoginHandler):
-    """Request additional scope on GitHub token.
+
+
+class NCSALoginHandler(oauthenticator.CILogonLoginHandler):
+    """Request additional scope on CILogon token.
     """
-    scope = ['public_repo', 'read:org', 'user:email']
+    scope = ['openid', 'email', 'profile', 'org.cilogon.userinfo']
+    skin = "LSST"
+    idp = "https://idp.ncsa.illinois.edu/idp/shibboleth"
 
 
 # Enable the authenticator to spawn with additional information acquired
 # with token with larger-than-default scope.
-class LSSTAuth(oauthenticator.GitHubOAuthenticator):
+class LSSTAuth(oauthenticator.CILogonOAuthenticator):
     """Authenticator to use our custom environment settings.
     """
     enable_auth_state = True
 
     _state = None
 
-    login_handler = LSSTLoginHandler
+    login_handler = NCSALoginHandler
 
     @gen.coroutine
     def pre_spawn_start(self, user, spawner):
@@ -148,78 +135,9 @@ class LSSTAuth(oauthenticator.GitHubOAuthenticator):
         if not self.enable_auth_state:
             return
         auth_state = yield user.get_auth_state()
-        gh_user = auth_state.get("github_user")
-        gh_token = auth_state.get("access_token")
-        if gh_user:
-            gh_id = gh_user.get("id")
-        gh_org = yield self._get_user_organizations(gh_token)
-        gh_email = gh_user.get("email")
-        if not gh_email:
-            gh_email = yield self._get_user_email(gh_token)
-        if gh_email:
-            spawner.environment['GITHUB_EMAIL'] = gh_email
-        gh_name = gh_user.get("name")
-        if not gh_name:
-            gh_name = gh_user.get("login")
-        if gh_id:
-            spawner.environment['EXTERNAL_UID'] = str(gh_id)
-        if gh_org:
-            orglstr = ""
-            for k in gh_org:
-                if orglstr:
-                    orglstr += ","
-                orglstr += k + ":" + str(gh_org[k])
-            spawner.environment['EXTERNAL_GROUPS'] = orglstr
-        if gh_name:
-            spawner.environment['GITHUB_NAME'] = gh_name
-        if gh_token:
-            spawner.environment['GITHUB_ACCESS_TOKEN'] = "[secret]"
-            self.log.info("Spawned environment: %s", json.dumps(
-                spawner.environment, sort_keys=True, indent=4))
-            spawner.environment['GITHUB_ACCESS_TOKEN'] = gh_token
-
-    @gen.coroutine
-    def _get_user_organizations(self, access_token):
-        """Get list of orgs user is a member of.  Requires 'read:org'
-        token scope.
-        """
-
-        http_client = AsyncHTTPClient()
-        headers = _api_headers(access_token)
-        next_page = "https://%s/user/orgs" % (GITHUB_API)
-        orgmap = {}
-        while next_page:
-            req = HTTPRequest(next_page, method="GET", headers=headers)
-            try:
-                resp = yield http_client.fetch(req)
-            except HTTPError:
-                return None
-            resp_json = json.loads(resp.body.decode('utf8', 'replace'))
-            next_page = next_page_from_links(resp)
-            for entry in resp_json:
-                # This could result in non-unique groups, if the first 32
-                #  characters of the group names are the same.
-                normalized_group = entry["login"][:32]
-                orgmap[normalized_group] = entry["id"]
-        return orgmap
-
-    @gen.coroutine
-    def _get_user_email(self, access_token):
-        """Determine even private email, if the token has 'user:email'
-        scope."""
-        http_client = AsyncHTTPClient()
-        headers = _api_headers(access_token)
-        next_page = "https://%s/user/emails" % (GITHUB_API)
-        while next_page:
-            req = HTTPRequest(next_page, method="GET", headers=headers)
-            resp = yield http_client.fetch(req)
-            resp_json = json.loads(resp.body.decode('utf8', 'replace'))
-            next_page = next_page_from_links(resp)
-            for entry in resp_json:
-                if "email" in entry:
-                    if "primary" in entry and entry["primary"]:
-                        return entry["email"]
-        return None
+        self.log.info("auth_state: %s", json.dumps(auth_state,
+                                                   indent=4,
+                                                   sort_keys=True))
 
 
 # Spawn the pod with custom settings retrieved via token additional scope.
@@ -296,7 +214,7 @@ class LSSTSpawner(kubespawner.KubeSpawner):
             if auth_state["id"] != self.user.id:
                 self.log.info("Updating userid from %d to %d" %
                               (self.user.id, auth_state["id"]))
-                #self.user.id = auth_state["id"]
+                # self.user.id = auth_state["id"]
                 # I think this messes up auth_token.
         pod_name = self._expand_user_properties(pn_template)
         self.pod_name = pod_name
@@ -340,13 +258,12 @@ c.JupyterHub.spawner_class = LSSTSpawner
 
 # Set up auth environment
 c.LSSTAuth.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
-c.LSSTAuth.client_id = os.environ['GITHUB_CLIENT_ID']
-c.LSSTAuth.client_secret = os.environ['GITHUB_CLIENT_SECRET']
-c.LSSTAuth.github_organization_whitelist = set(
-    (os.environ['GITHUB_ORGANIZATION_WHITELIST'].split(",")))
+c.LSSTAuth.client_id = os.environ['CILOGON_CLIENT_ID']
+c.LSSTAuth.client_secret = os.environ['CILOGON_CLIENT_SECRET']
 
 # Set options form
-c.LSSTSpawner.options_form = optform
+if len(imagelist) > 1:
+    c.LSSTSpawner.options_form = optform
 
 
 # Listen to all interfaces

9c6aa0a162ba1d154863cec8e4946fb0fad2f318
adam <athornton@gmail.com>
2017-09-21 17:25:33 -0700

    Restrict to one hub interface.

--- jupyterhub/hublauncher.sh
+++ jupyterhub/hublauncher.sh
@@ -13,6 +13,6 @@ chown -R ${USER}:${GROUP} ${HOMEDIR}
 
 cd ${HOMEDIR}
 export HUB_BIND_IP=$(/sbin/ifconfig | grep 'inet ' | awk '{print $2}' | \
-			 grep -v '127.0.0.1')
+			 grep -v '127.0.0.1' | head -1)
 exec sudo -E -u ${USER} /usr/bin/jupyterhub --debug -f \
      /opt/lsst/software/jupyterhub/config/jupyterhub_config.py

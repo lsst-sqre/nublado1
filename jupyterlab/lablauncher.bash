9b673f2ff2e88421d00b9edf82f1365e09d03b6a
adam <athornton@gmail.com>
2018-03-28 17:04:17 -0700

    Add some of the things.

--- jupyterlab/lablauncher.bash
+++ jupyterlab/lablauncher.bash
@@ -147,7 +147,8 @@ function change_staging_id() {
     # JupyterLab wants to rebuild the index for extensions.
     # If the files it wants exist and are owned by another user, it fails
     #  even if they are writeable.
-    local stagedir="/opt/rh/rh-python36/root/usr/share/jupyter/lab/staging"
+    local stagedir="/opt/rh/rh-python36/root/usr/lib/python3.6"
+    stagedir="${stagedir}/site-packages/jupyterlab/staging"
     for i in package.json index.js webpack.config.js; do
 	chown ${U_NAME} "${stagedir}/${i}"
     done

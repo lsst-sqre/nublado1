ae4457b600df73413014db5cdec3502bd92f4b6e
adam <athornton@gmail.com>
2018-03-30 15:22:57 -0700

    Move virtualenvwrapper after initialization of Python SCL.

--- jupyterlab/local08-virtualenvwrapper.sh
+++ jupyterlab/local08-virtualenvwrapper.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # Source virtualenvwrapper.sh
+# This must happen *after* the python scl is enabled.
 if [ $(id -u) -ne 0 ]; then
-    source /usr/bin/virtualenvwrapper.sh
+    source $(which virtualenvwrapper.sh)
     rc=$?
     if [ "${rc}" -ne 0 ]; then
 	alias python='python3'
-	source /usr/bin/virtualenvwrapper.sh
+	source $(which virtualenvwrapper.sh)
 	unalias python
     fi
 fi

d5ea1eb40513f737b6db42307f38ebbb23f66687
adam <athornton@gmail.com>
2018-05-30 16:03:05 -0700

    Add PATH/INFOPATH

--- jupyterlab/local05-path.sh
+++ jupyterlab/local05-path.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 PATH=/usr/local/texlive/2018/bin/x86_64-linux:/usr/local/bin:$PATH
 PATH=$HOME/bin:$HOME/.local/bin:$PATH
-export PATH
+MANPATH=Add /usr/local/texlive/2018/texmf-dist/doc/man:/usr/local/share/man:$MANPATH
+INFOPATH=/usr/local/texlive/2018/texmf-dist/doc/info:/usr/local/share/info:$INFOPATH
+export PATH MANPATH INFOPATH

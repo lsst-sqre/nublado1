f6ce5a25907e5e04ac71bd9517547e0bedfdd1af
Joshua Hoblitt <josh@hoblitt.com>
2018-12-26 16:00:18 -0700

    fix hub cm name

--- tools/terraform/modules/jupyterhub/configmap.tf
+++ tools/terraform/modules/jupyterhub/configmap.tf
@@ -25,7 +25,7 @@ data "local_file" "30-environment_py" {
 
 resource "kubernetes_config_map" "jupyterhub_config" {
   metadata {
-    name      = "jupyterhub-config"
+    name      = "jld-hub-config"
     namespace = "${var.namespace}"
   }
 

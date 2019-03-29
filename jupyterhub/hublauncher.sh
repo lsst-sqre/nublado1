65d15e018cde9de9ca1248a9bedb1b378e58da79
adam <athornton@gmail.com>
2019-03-29 12:05:11 -0700

    Make Hub and Prepuller not run as root.

--- jupyterhub/hublauncher.sh
+++ jupyterhub/hublauncher.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
-USER=jupyter
-GROUP=jupyter
-HOMEDIR=/home/${USER}
-if ! [ -d ${HOMEDIR} ]; then
-    mkdir -p ${HOMEDIR}
+if [ $(id -u) -eq 0 ]; then
+    echo 1>&2 "Warning: running as UID 0."
+    echo 1>&2 "Sleeping to prevent respawn spamming."
+    sleep 60
+    exit 2
 fi
+cd ${HOME}
 dbtype=$(echo ${SESSION_DB_URL} | cut -d ':' -f 1)
 if [ "${dbtype}" == "sqlite" ]; then
-    if ! [ -f ${HOMEDIR}/jupyterhub.sqlite ]; then
-        touch ${HOMEDIR}/jupyterhub.sqlite
+    if ! [ -f ./jupyterhub.sqlite ]; then
+        touch ./jupyterhub.sqlite
     fi
-    chmod 0600 ${HOMEDIR}/jupyterhub.sqlite
+    chmod 0600 ./jupyterhub.sqlite
 fi
-chown -R ${USER}:${GROUP} ${HOMEDIR}
-cd ${HOMEDIR}
 dbgflag=""
 jhdir="/opt/lsst/software/jupyterhub"
 conf="${jhdir}/config/jupyterhub_config.py"
 if [ -n "${DEBUG}" ]; then
     dbgflag="--debug "
 fi
-if [ -f "/home/jupyter/jupyterhub-proxy.pid" ]; then
-    rm /home/jupyter/jupyterhub-proxy.pid
+if [ -f "${HOME}/jupyterhub-proxy.pid" ]; then
+    rm "${HOME}/jupyterhub-proxy.pid"
 fi
-cmd="sudo -E -u ${USER} /usr/local/bin/jupyterhub ${dbgflag} -f ${conf}"
+cmd="/usr/local/bin/jupyterhub ${dbgflag} -f ${conf}"
 echo $cmd
 if [ -n "${DEBUG}" ]; then
     ${cmd}

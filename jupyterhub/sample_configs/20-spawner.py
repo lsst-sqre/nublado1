61372975f8263010fdcd09d7457ae8e05ed23fa2
adam <athornton@gmail.com>
2019-07-31 15:04:11 -0700

    Update spawner for JH utils 0.6.0

--- jupyterhub/sample_configs/20-spawner.py
+++ jupyterhub/sample_configs/20-spawner.py
@@ -70,6 +70,7 @@ class LSSTSpawner(namespacedkubespawner.NamespacedKubeSpawner):
         dailies = int(os.getenv("PREPULLER_DAILIES") or 3)
         weeklies = int(os.getenv("PREPULLER_WEEKLIES") or 2)
         releases = int(os.getenv("PREPULLER_RELEASES") or 1)
+        cachefile = os.getenv("HOME") + "/repo-cache.json"
         self._set_custom_user_resources()
         scanner = ScanRepo(host=host,
                            owner=owner,
@@ -79,6 +80,7 @@ class LSSTSpawner(namespacedkubespawner.NamespacedKubeSpawner):
                            weeklies=weeklies,
                            releases=releases,
                            json=True,
+                           cachefile=cachefile
                            )
         try:
             scanner.scan()
@@ -93,8 +95,6 @@ class LSSTSpawner(namespacedkubespawner.NamespacedKubeSpawner):
             all_tags = scanner.get_all_tags()
         except AttributeError:
             all_tags = []
-        if lnames[0].endswith(":recommended"):
-            ldescs[0] = self._match_recommended(scanner)
         optform = "<label for=\"%s\">%s</label><br />\n" % (title, title)
         now = datetime.datetime.now()
         nowstr = now.ctime()
@@ -205,26 +205,37 @@ class LSSTSpawner(namespacedkubespawner.NamespacedKubeSpawner):
         try:
             gnames = self._get_user_groupnames()
             uname = self.user.name
+            self.log.debug("User '{}' / Groups '{}'".format(uname, gnames))
             with open(rfile, "r") as rf:
                 resmap = json.load(rf)
             for resdef in resmap:
                 apply = False
                 if resdef.get("disabled"):
+                    self.log.debug(
+                        "Skipping disabled resource map {}.".format(resdef))
                     continue
                 candidate = resdef.get("resources")
                 if not candidate:
+                    self.log.debug(
+                        "No resources in candidate {}".format(candidate))
                     continue
+                self.log.debug(
+                    "Considering candidate resource map {}".format(resdef))
                 ruser = resdef.get("user")
                 rgroup = resdef.get("group")
                 if ruser and ruser == uname:
+                    self.log.debug("User resource map match.")
                     apply = True
                 if rgroup and rgroup in gnames:
+                    self.log.debug("Group resource map match.")
                     apply = True
                 if apply:
                     for fld in ["size_index", "cpu_quota", "mem_quota"]:
                         vv = candidate.get(fld)
                         if vv and vv > resources[fld]:
                             resources[fld] = vv
+                    self.log.info(
+                        "Setting custom resources '{}'".format(resources))
                     self._custom_resources = resources
         except Exception as exc:
             self.log.error(
@@ -237,95 +248,6 @@ class LSSTSpawner(namespacedkubespawner.NamespacedKubeSpawner):
         except AttributeError:
             self.log.error("Authenticator object has no groups attribute.")
             return []
-
-    def _match_recommended(self, scanner):
-        (lnames, ldescs) = scanner.extract_image_info()
-        ltags = [ x.split(':')[-1] for x in lnames]
-        if self.recommended_tag:
-            self.log.debug("recommended is: {}".format(self.recommended_tag))
-            if self.recommended_tag == "NOTFOUND":
-                return ldescs[0]
-            if self.recommended_tag in ltags:
-                idx = -1
-                try:
-                    idx = ltags.index(self.recommended_tag)
-                except ValueError:
-                    pass
-                if idx != -1:
-                    return "Recommended (%s)" % ldescs[idx]
-        self.log.debug("Searching for recommended tag.")
-        # We know lnames[0] ends with ":recommended"
-        # ldescs[0] is where we started (probably "Recommended")
-        authtok = self._get_auth_token(scanner)
-        if not authtok:
-            return ldescs[0]
-        baseurl = self._get_baseurl(scanner)
-        if not baseurl:
-            return ldescs[0]
-        headers = {}
-        if authtok != "NO-AUTH":
-            headers = {"Authorization": "Bearer {}".format(authtok)}
-        self.log.debug("Getting manifest for 'recommended' tag.")
-        resp = requests.get(baseurl + "manifests/recommended",
-                            headers=headers, json=True)
-        if resp:
-            recjson = resp.json()
-        else:
-            recjson = {}
-        digest = self._get_layers(recjson)
-        if not digest:
-            return ldescs[0]
-        # Now we have the hash of "recommended"; next we match it to
-        #  another tag.
-        ltags = [x.split(":")[-1] for x in lnames]
-        for ltag in ltags:
-            if ltag == "recommended":
-                continue
-            idx = ltags.index(ltag)
-            self.log.debug("Getting manifest for '{}' tag.".format(ltag))
-            tag = "manifests/" + ltag
-            resp = requests.get(baseurl + tag, headers=headers, json=True)
-            try:
-                recjson = resp.json()
-            except json.decoder.JSONDecodeError as exc:
-                self.log.error("Failed to decode JSON: {}".format(exc))
-                self.log.error("Message body: {}".format(resp.text()))
-                continue
-            imgdigest = self._get_layers(recjson)
-            if imgdigest and imgdigest == digest:
-                self.recommended_tag = ltag
-                self.log.debug("Match: {} is 'recommended'.".format(ltag))
-                return "Recommended (%s)" % ldescs[idx]
-        # Not in our displayed images.  Try the whole list...
-        self.log.debug("Recommended tag not in displayed images.")
-        all_tags = []
-        try:
-            all_tags = scanner.get_all_tags()
-        except AttributeError:
-            return ldescs[0]
-        for tag in all_tags:
-            if tag == "recommended":
-                continue
-            self.log.debug("Getting manifest for '{}' tag.".format(tag))
-            mtag = "manifests/" + tag
-            resp = requests.head(baseurl + mtag, headers=headers,
-                                 json=True)
-            if resp:
-                try:
-                    recjson = resp.json()
-                except json.decoder.JSONDecodeError as exc:
-                    self.log.error("Failed to decode JSON: {}".format(exc))
-                    self.log.error("Message body: {}".format(resp.text()))
-                    continue
-            else:
-                recjson = {}
-            imgdigest = self._get_layers(recjson)
-            if imgdigest and imgdigest == digest:
-                self.recommended_tag = tag
-                return "Recommended (%s)" % tag
-        # We didn't find it.
-        self.recommended_tag = "NOTFOUND"
-        return ldescs[0]
 
     def _get_layers(self, recjson):
         fsl = recjson.get("fsLayers")

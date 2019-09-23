546ddce3950f9ff363f94c7150f83096e6458da7
adam <athornton@gmail.com>
2019-09-23 16:46:45 -0700

    Show motd whether or not it's a login shell.

--- jupyterlab/local03-showmotd.sh
+++ jupyterlab/local03-showmotd.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
-shopt -q login_shell
-rc=$?
-if [ ${rc} -ne 0 ]; then
-    case $- in
-	*i*) cat /etc/motd
-	     ;;
-	*)
-	     ;;
-    esac
-fi
+case $- in
+    *i*) cat /etc/motd
+        ;;
+    *)
+        ;;
+esac

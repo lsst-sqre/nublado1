3b54a4363437d54b948459a139fd3477d07b9bf1
adam <athornton@gmail.com>
2019-11-22 14:37:51 -0700

    Fix apiGroup for rolebindings.

--- tools/terraform/modules/prepuller/clusterrolebinding.tf
+++ tools/terraform/modules/prepuller/clusterrolebinding.tf
@@ -10,7 +10,7 @@ resource "kubernetes_cluster_role_binding" "prepuller" {
   }
 
   role_ref {
-    api_group = ""
+    api_group = "rbac.authorization.k8s.io"
     name      = "prepuller"
     kind      = "ClusterRole"
   }

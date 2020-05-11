9555466c44040ffd738deec3d7a61064e89dea4f
adam <athornton@gmail.com>
2020-05-11 15:57:54 -0700

    Tweak startup

--- jupyterlab/runlab.sh
+++ jupyterlab/runlab.sh
@@ -160,6 +160,10 @@ if [ -n "${HUB_SERVICE_HOST}" ]; then
 fi
 export JUPYTERHUB_API_URL
 # Set Firefly URL and landing page
+if [ -z "${EXTERNAL_URL}" ]; then
+    EXTERNAL_URL=${EXTERNAL_INSTANCE_URL}
+    export EXTERNAL_URL
+fi
 host_url=$(echo ${EXTERNAL_URL} | cut -d '/' -f 1-3)
 FIREFLY_ROUTE=${FIREFLY_ROUTE:-"/firefly/"}
 FIREFLY_URL="${host_url}${FIREFLY_ROUTE}"

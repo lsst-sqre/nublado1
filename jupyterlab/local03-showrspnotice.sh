322bde3adbb77b11fbd387b6b139035164498ece
adam <athornton@gmail.com>
2020-09-23 16:48:32 -0700

    Updates

--- jupyterlab/local03-showrspnotice.sh
+++ jupyterlab/local03-showrspnotice.sh
@@ -5,18 +5,22 @@ case "$-" in
         # OK, we're interactive.
         #  Are we a login shell?
         if shopt -q login_shell; then
-	    # Yes.  Display the notice(s)
+            # Yes.  Display the notice(s)
             if [ -e "/usr/local/etc/rsp_notice" ]; then
                 cat /usr/local/etc/rsp_notice
-	    fi
-            msgdir="/opt/lsst/software/jupyterlab/messages.d"
-            if [ -e  ${msgdir} ]; then
-                any=$(ls ${msgdir})
-                if [ -n "${any}" ]; then
-                    cat ${msgdir}/*
-                fi
             fi
-	fi
+            msgdir="/opt/lsst/software/jupyterlab/messages.d"
+            if [ -e ${msgdir} ]; then
+                for i in $(ls -t ${msgdir}); do
+                    if [ -e "${i}" ]; then
+                        # list modification time
+                        date -r "${i}"
+                        echo "------------------------------"
+                        cat "${i}"
+                    fi
+                done
+            fi
+        fi
         ;;
     *)
         ;;

164aff48ba5799945e373cce64982923c0a8c774
adam <athornton@lsst.org>
2021-04-28 12:13:52 -0700

    Make JL work with T&S stack builds too

--- jupyterlab/lsstlaunch.bash
+++ jupyterlab/lsstlaunch.bash
@@ -1,7 +1,16 @@
 #!/bin/bash
 CONFIG_FILE=$1
-source /opt/lsst/software/stack/loadLSST.bash
-setup lsst_distrib
+# /opt/lsst/sal only exists in SAL builds of the stack
+if [ -d /opt/lsst/sal ]; then
+    source /opt/lsst/sal/salbldsteps.bash 2>&1 > /dev/null
+    for i in xml idl sal salobj ATDome ATDomeTrajectory ATMCSSimulator \
+            simactuators standardscripts scriptqueue externalscripts ; do
+        setup ts_${i} -t current
+    done
+else
+    source /opt/lsst/software/stack/loadLSST.bash
+    setup lsst_distrib
+fi
 setup display_firefly
 if [ -e ${HOME}/notebooks/.user_setups ]; then
     source ${HOME}/notebooks/.user_setups
